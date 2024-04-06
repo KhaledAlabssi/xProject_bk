45d9b898cfdce59201eb5aa42818a94b5d85d5b6
KhaledAlabssi <khaled.absi5@gmail.com>
2024-04-06 17:44:55 +0200

    finalize update profile functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,26 @@
 import User from "../models/user.js"
 import CustomError from "../errors/customError.js"
-import { isTokenValid } from "../util/jwt.js"
+import { isTokenValid, tokenToResponse } from "../util/jwt.js"
 
 
 const getProfile = async (req, res) => {
-    
-    res.status(200).json({user: req.user, success: true})
+
+    res.status(200).json({ user: req.user, success: true })
 }
 
 const updateProfile = async (req, res) => {
+    const { email, name } = req.body;
+    if (!email || !name) {
+        throw new CustomError("Please provide all values")
+    }
+    const user = User.findOne({ _id: req.user.userId })
+    user.email = email
+    user.name = name
+    await user.save()
+    const tokenInfo = { name: user.name, userId: user._id, role: user.role }
+    tokenToResponse({res, user: tokenInfo})
     
-    
-    res.send("update user")
+    res.status(200).json({msg: "User updated successfully", success: true})
 }
 
 const updatePassword = async (req, res) => {
@@ -26,8 +35,8 @@ const updatePassword = async (req, res) => {
     }
     user.password = newPassword
     await user.save()
-    
-    res.status(200).json({msg: "Password updated successfully", success: true})
+
+    res.status(200).json({ msg: "Password updated successfully", success: true })
 }
 
-export {getProfile, updateProfile, updatePassword}
+export { getProfile, updateProfile, updatePassword }
